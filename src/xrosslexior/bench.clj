e8427723e53865ea69fc07ce415770a1f8a13ae2
Zack M. Davis <code@zackmdavis.net>
2015-01-31 14:47:42 -0800

    more simple benchmarking tools
    xrosslexior.bench=> (time (convenient-experiment 3 50))
    "Elapsed time: 258140.711058 msecs"
    {:old {:avrg 0.8494501573000002, :stdv 2.483122732354644}, :new {:avrg
    4.281548738359999, :stdv 10.151301940796944}}
    xrosslexior.bench=> (time (convenient-experiment 3 50))
    "Elapsed time: 233648.8898 msecs"
    {:old {:avrg 0.9826805831800003, :stdv 1.6025643821713615}, :new {:avrg
    3.6901217727399995, :stdv 6.782018468566562}}
    xrosslexior.bench=> (time (convenient-experiment 3 50))
    "Elapsed time: 258403.75089 msecs"
    {:old {:avrg 0.7159657852000001, :stdv 1.5485036215595984}, :new {:avrg
    4.451975969680001, :stdv 7.979775996210098}}

--- src/xrosslexior/bench.clj
+++ src/xrosslexior/bench.clj
@@ -5,9 +5,39 @@
   `(let [start# (. System (nanoTime))
          evaluated# ~code
          stop# (. System (nanoTime))]
-     {:result evaluated#
+     {:output evaluated#
       :time (/ (- stop# start#) 1e9)}))
 
+(defn square-grid-seeded-with-word [word]
+  (write-row (empty-square-grid (count word)) 0 word))
+
 (defn benchmark-first-row-words [solver words]
-  (let [grids (map #(write-row (empty-square-grid (count %)) 0 %) words)]
+  (let [grids (map square-grid-seeded-with-word words)]
     (map #(benchmark (solver %)) grids)))
+
+(defn mean [data]
+  (/ (reduce + data) (count data)))
+
+(defn sample-variance [data]
+  (- (/ (reduce + (map #(Math/pow % 2) data))
+        (count data))
+     (Math/pow (mean data) 2)))
+
+(defn sample-standard-deviation [data]
+  (Math/sqrt (sample-variance data)))
+
+(defn summarize-benchmark [results]
+  (let [times (map #(% :time) results)]
+      {:avrg (mean times)
+       :stdv (sample-standard-deviation times)}))
+
+(defn convenient-experiment [n size]
+  (let [start (rand-int 300)
+        words (take size (drop start (n-dictionary n)))
+        results (into {}
+                      (for [[name solver] [[:old solve] [:new solve-it]]]
+                        [name (benchmark-first-row-words solver words)]))
+        summary (into {}
+                      (for [[solver-name result] results]
+                        [solver-name (summarize-benchmark result)]))]
+    summary))
