23a8bacfcd51454875a9777756b12fc3abed13e9
Zack M. Davis <code@zackmdavis.net>
2015-02-08 20:24:49 -0800

    check in some more patterns

--- src/xrosslexior/patterns.clj
+++ src/xrosslexior/patterns.clj
@@ -1,5 +1,6 @@
 (ns xrosslexior.patterns)
 
+
 (def seven-eleven-pattern  [[nil nil nil nil nil :█  nil nil nil nil nil]
                             [nil nil nil nil nil :█  nil nil nil nil nil]
                             [nil nil nil nil :█  nil nil nil nil nil nil]
@@ -7,6 +8,20 @@
                             [:█  nil nil nil nil nil :█  nil nil nil :█ ]
                             [:█  nil nil nil nil :█  nil nil nil nil :█ ]
                             [:█  :█  nil nil nil :█  nil nil nil :█  :█ ]])
+
+(def thirteen-eleven-pattern  [[nil nil nil nil nil :█  nil nil nil nil nil]
+                               [nil nil nil nil nil :█  nil nil nil nil nil]
+                               [nil nil nil nil :█  nil nil nil nil nil nil]
+                               [nil nil nil :█  nil nil nil :█  nil nil nil]
+                               [:█  nil nil nil nil nil :█  nil nil nil :█ ]
+                               [:█  :█  nil nil nil :█  nil nil nil :█  :█ ]
+                               [:█  :█  nil nil nil :█  nil nil nil :█  :█ ]
+                               [nil nil :█  nil nil :█  nil nil :█  nil nil]
+                               [nil nil nil nil nil :█  nil nil nil nil nil]
+                               [nil nil nil nil :█  nil nil nil nil nil nil]
+                               [nil nil nil :█  nil nil nil :█  nil nil nil]
+                               [:█  nil nil nil nil nil :█  nil nil nil :█ ]
+                               [:█  :█  nil nil nil :█  nil nil nil :█  :█ ]])
 
 (def my-first-standard-size-pattern
   ;; after the classic Dell Puzzler's Crossword Puzzles March 2015 #5 easy
@@ -25,3 +40,40 @@
    [nil nil nil nil nil nil :█  nil nil nil nil :█  nil nil nil]
    [:█  nil nil nil nil nil :█  nil nil nil nil :█  nil nil nil]
    [:█  :█  nil nil nil nil :█  nil nil nil nil :█  nil nil nil]])
+
+(def standard-pattern-extra-black
+  ;; again as Dell's March 2015 #5 but with extra black squares
+  ;; inserted
+  [[nil nil nil :█  :█  nil nil nil :█  nil nil nil :█  :█  :█ ]
+   [nil nil nil :█  nil nil nil nil :█  nil nil nil nil nil :█ ]
+   [nil nil nil :█  nil nil nil nil :█  nil nil nil nil nil :█ ]
+   [:█  :█  nil nil nil nil :█  nil nil nil nil :█  nil nil nil]
+   [:█  nil nil nil nil :█  :█  nil nil nil :█  nil nil nil nil]
+   [nil nil nil nil :█  nil nil nil nil :█  nil nil nil nil nil]
+   [nil nil nil :█  nil nil nil nil :█  nil nil nil :█  :█  :█ ]
+   [nil nil nil nil nil nil nil :█  nil nil nil nil nil nil nil]
+   [:█  :█  :█  nil nil nil :█  nil nil nil nil :█  nil nil nil]
+   [nil nil nil nil nil :█  nil nil nil nil :█  nil nil nil nil]
+   [nil nil nil nil :█  nil nil nil :█  :█  nil nil nil nil :█ ]
+   [nil nil nil :█  nil nil nil nil :█  nil nil nil nil :█  :█ ]
+   [:█  nil nil nil nil nil :█  nil nil nil nil :█  nil nil nil]
+   [:█  nil nil nil nil nil :█  nil nil nil nil :█  nil nil nil]
+   [:█  :█  :█  nil nil nil :█  nil nil nil :█  :█  nil nil nil]])
+
+(def another-standard-pattern
+  ;; after Dell Puzzler's Crossword Puzzles March 2015 #1 easy
+  [[nil nil nil :█  :█  :█  nil nil nil :█  :█  :█  nil nil nil]
+   [nil nil nil nil :█  nil nil nil nil nil :█  nil nil nil nil]
+   [nil nil nil nil :█  nil nil nil nil nil :█  nil nil nil nil]
+   [nil nil nil nil nil nil nil nil :█  nil nil nil nil nil :█ ]
+   [:█  nil nil nil nil nil nil :█  nil nil nil nil :█  :█  :█ ]
+   [:█  :█  :█  nil nil nil :█  nil nil nil nil nil nil nil nil]
+   [nil nil nil :█  :█  :█  nil nil nil nil :█  nil nil nil nil]
+   [nil nil nil nil :█  nil nil nil nil nil :█  nil nil nil nil]
+   [nil nil nil nil :█  nil nil nil nil :█  :█  :█  nil nil nil]
+   [nil nil nil nil nil nil nil nil :█  nil nil nil :█  :█  :█ ]
+   [:█  :█  :█  nil nil nil nil :█  nil nil nil nil nil nil :█ ]
+   [:█  nil nil nil nil nil :█  nil nil nil nil nil nil nil nil]
+   [nil nil nil nil :█  nil nil nil nil nil :█  nil nil nil nil]
+   [nil nil nil nil :█  nil nil nil nil nil :█  nil nil nil nil]
+   [nil nil nil :█  :█  :█  nil nil nil :█  :█  :█  nil nil nil]])
