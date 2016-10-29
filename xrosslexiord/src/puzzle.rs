b850cf847850620a6c4134d05fd2b94f4f1a55fa
Zack M. Davis <code@zackmdavis.net>
2016-10-29 14:47:51 -0700

    wordspan addresses across
    Naturally, we'll want to generalize the wordspan-finding code, rather
    than copy-paste-rewriting it with the roles of `row` and `col` reversed.

--- xrosslexiord/src/puzzle.rs
+++ xrosslexiord/src/puzzle.rs
@@ -1,12 +1,12 @@
 #![allow(dead_code)]
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum Orientation {
     Across,
     Down
 }
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct WordspanAddress {
     start_row: usize,
     start_col: usize,
@@ -37,11 +37,11 @@ pub struct Puzzle {
 
 
 impl Puzzle {
-    fn read(&self, row_index: usize, col_index: usize) -> char {
+    pub fn read(&self, row_index: usize, col_index: usize) -> char {
         self.backing[self.cols*row_index + col_index]
     }
 
-    fn write(&mut self, row_index: usize, col_index: usize,
+    pub fn write(&mut self, row_index: usize, col_index: usize,
              letter: char) {
         self.backing[self.cols*row_index + col_index] = letter;
     }
@@ -75,6 +75,45 @@ impl Puzzle {
         }
     }
 
+    fn wordspan_addresses_across(&self) -> Vec<WordspanAddress> {
+        let mut addresses = Vec::new();
+        for row in 0..self.rows {
+            let mut open_word_from: Option<usize> = None;
+            for col in 0..self.cols {
+                match open_word_from {
+                    None => {
+                        if self.read(row, col) == BARRIER {
+                            continue;
+                        } else {
+                            open_word_from = Some(col);
+                        }
+                    },
+                    Some(open_from) => {
+                        if self.read(row, col) == BARRIER {
+                            let address = WordspanAddress::new(
+                                row, open_from, Orientation::Across,
+                                col-open_from
+                            );
+                            addresses.push(address);
+                            open_word_from = None;
+                        } else {
+                            continue;
+                        }
+                    }
+                }
+            }
+            // close off word closed by edge of puzzle rather than barrier
+            if let Some(open_from) = open_word_from {
+                let address = WordspanAddress::new(
+                    row, open_from, Orientation::Across,
+                    self.cols-open_from
+                );
+                addresses.push(address);
+            }
+        }
+        addresses
+    }
+
 }
 
 
@@ -82,7 +121,7 @@ impl Puzzle {
 mod tests {
     use super::*;
 
-    fn test_puzzle() -> Puzzle {
+    fn test_puzzle_i() -> Puzzle {
         Puzzle {
             rows: 5,
             cols: 5,
@@ -96,23 +135,39 @@ mod tests {
         }
     }
 
+    fn test_puzzle_ii() -> Puzzle {
+        Puzzle {
+            rows: 7,
+            cols: 11,
+            backing: vec![
+                'S', 'W', 'I', 'F', 'T', '█', 'S', 'T', 'A', 'C', 'K',
+                'L', 'A', 'M', 'A', 'S', '█', 'L', 'A', 'M', 'A', 'R',
+                'A', 'L', 'A', 'N', '█', 'L', 'E', 'G', 'A', 'T', 'O',
+                'B', 'E', 'G', '█', 'L', 'A', 'D', '█', 'D', 'E', 'C',
+                '█', 'S', 'E', 'L', 'I', 'M', '█', 'F', 'E', 'R', '█',
+                '█', 'A', 'R', 'E', 'S', '█', 'P', 'I', 'U', 'S', '█',
+                '█', '█', 'Y', 'I', 'P', '█', 'L', 'E', 'S', '█', '█',
+            ]
+        }
+    }
+
     #[test]
     fn concerning_read_wordspan_across() {
         let write_address = WordspanAddress::new(0, 0, Orientation::Across, 5);
-        assert_eq!(test_puzzle().read_wordspan(write_address),
+        assert_eq!(test_puzzle_i().read_wordspan(write_address),
                    vec!['W', 'R', 'I', 'T', 'E']);
     }
 
     #[test]
     fn concerning_read_wordspan_down() {
         let raise_address = WordspanAddress::new(0, 1, Orientation::Down, 5);
-        assert_eq!(test_puzzle().read_wordspan(raise_address),
+        assert_eq!(test_puzzle_i().read_wordspan(raise_address),
                    vec!['R', 'A', 'I', 'S', 'E']);
     }
 
     #[test]
     fn concerning_write_wordspan_across() {
-        let mut puzzle = test_puzzle();
+        let mut puzzle = test_puzzle_i();
         // in more ways than one
         let write_address = WordspanAddress::new(0, 0, Orientation::Across, 5);
         puzzle.write_wordspan(write_address, vec!['P', 'L', 'A', 'T', 'E']);
@@ -122,11 +177,24 @@ mod tests {
 
     #[test]
     fn concerning_write_wordspan_down() {
-        let mut puzzle = test_puzzle();
+        let mut puzzle = test_puzzle_i();
         let entry_address = WordspanAddress::new(0, 4, Orientation::Down, 5);
         puzzle.write_wordspan(entry_address, vec!['F', 'A', 'I', 'T', 'H']);
         assert_eq!(puzzle.read_wordspan(entry_address),
                    vec!['F', 'A', 'I', 'T', 'H']);
     }
 
+    #[test]
+    fn concerning_wordspan_addresses_across() {
+        let puzzle = test_puzzle_ii();
+        assert_eq!(
+            &[WordspanAddress::new(0, 0, Orientation::Across, 5),
+              WordspanAddress::new(0, 6, Orientation::Across, 5),
+              WordspanAddress::new(1, 0, Orientation::Across, 5),
+              WordspanAddress::new(1, 6, Orientation::Across, 5),
+              WordspanAddress::new(2, 0, Orientation::Across, 4),
+              WordspanAddress::new(2, 5, Orientation::Across, 6)],
+            &puzzle.wordspan_addresses_across()[..6]
+        );
+    }
 }
